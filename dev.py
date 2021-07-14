531205cb1fcffda5b006f133df6f9e2250d85b2d
Paul Melnikow <email@paulmelnikow.com>
2021-07-14 14:36:01 -0400

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S poetry run python
 
 import os
 import click
@@ -24,8 +24,8 @@ def cli():
 
 
 @cli.command()
-def init():
-    execute("pip3 install --upgrade -r requirements_dev.txt")
+def install():
+    execute("poetry install --remove-untracked")
 
 
 @cli.command()
